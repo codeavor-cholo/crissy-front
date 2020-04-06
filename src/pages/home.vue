d2cd01c830e592e3b8897481ff3f2ba90055209f
raph <paengui17@gmail.com>
2020-04-06 20:19:09 +0800

    reservation mobile

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,14 @@
 <template>
   <q-layout view="hHh lpR fff">
 
+<!-- START OF DESKTOP HEADER -->
+    <div class="desktop-only">
     <q-header class="bg-grey-11 text-white row items-center" style="height:63px">
       <q-toolbar style="padding-left:100px">
-        
-         
+                
         <img style="height:100%;width:70px" src="statics/pics/logo.png">
-          
-        
+                 
         <div class="row justify-between">
-
           <div class=" text-orange-4 q-pl-lg">
             <q-tabs v-model="tab">
             <q-route-tab name="ho"  to="/" ><b>Home</b></q-route-tab>
@@ -48,12 +47,72 @@
         </div>
       </q-toolbar>
     </q-header>
+    </div>
+<!-- END OF DESKTOP HEADER -->
 
+<!-- START OF MOBILE HEADER -->
+    <div class="mobile-only">
+    <q-header class="bg-grey-11 text-white q-pa-sm" style="height:63px">
+      <q-toolbar>
+        <div class="row items-center">        
+        <div><img style="height:100%;width:60px" src="statics/pics/logo.png"></div>
+        <div><div class="q-pl-md row items-center q-gutter-md">
+          <q-btn-dropdown dense label="Account" v-show="!show" text-color="orange-8" flat="">
+            <q-list>
+              <q-item clickable v-close-popup @click="$router.push('/profile')">
+                <q-item-section>
+                  <q-item-label>My Account</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup @click="tempLogout">
+                <q-item-section>
+                  <q-item-label>Log Out</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>  
+          
+          <q-btn flat text-color="black" label="Login" v-show="show" @click="show=!show"/>
+          <q-input dense standout style="width:120px" v-model="search">
+            <template v-slot:append>
+              <q-icon name="search" color="orange" />
+            </template>
+          </q-input>
+          </div>
+          </div>
+          </div>
+      </q-toolbar>
+
+      <div class=" text-orange-4">
+            <q-tabs v-model="tab">
+            <q-route-tab name="ho"  to="/" ><b>Home</b></q-route-tab>
+            <q-route-tab name="men" to="/menu"><b>Menu</b></q-route-tab>
+            <q-route-tab name="gal" to="/"><b>Gallery</b></q-route-tab>
+            <q-route-tab name="pac" to="/packages"><b>Packages</b></q-route-tab>
+            </q-tabs>
+      </div>
+    </q-header>
+    </div>
+<!-- END OF MOBILE HEADER -->
+
+<!-- START OF DESKTOP PAGE -->
+    <div class="desktop-only">
     <q-page-container>
       <router-view style="padding-left:100px;padding-right:100px;padding-top:50px;padding-bottom:50px" />
     </q-page-container>
+    </div>
+<!-- END OF DESKTOP PAGE -->
 
-    
+<!-- START OF MOBILE PAGE -->
+    <div class="mobile-only">
+    <q-page-container>
+      <router-view style="padding-top:50px;padding-bottom:50px" />
+    </q-page-container>
+    </div>
+<!-- END OF MOBILE PAGE -->
+
+<!-- START OF DESKTOP FOOTER -->
+    <div class="desktop-only">
     <q-footer class="bg-black text-white">
       <q-toolbar>
         <q-toolbar-title>
@@ -72,7 +131,34 @@
         </q-toolbar-title>
       </q-toolbar>
     </q-footer>
+    </div>
+<!-- END OF DESKTOP FOOTER -->
 
+<!-- START OF MOBILE FOOTER -->
+    <div class="mobile-only">
+    <q-footer class="bg-black text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div class="column items-center text-orange-4 q-pt-sm">
+            <div style="font-size:16px;font-family: 'Josefin Sans', sans-serif;"><b>Crissy's Meal Magic Catering Services</b></div>
+            <div style="font-size:14px"><b>Maricris Subia Vetores</b></div>
+            <div class="row q-gutter-sm" style="font-size:13px">
+              <q-icon name="perm_phone_msg" class="text-primary" style="font-size: 1rem;" />
+              <div><b>0926 006 8364</b></div>
+            </div>
+            <div style="font-size:13px" class="row q-gutter-sm q-pb-sm">
+              <q-icon name="place" class="text-primary" style="font-size: 1rem;" />
+              <div class="column items-center">
+                <div><b>83 Pangasinan Street Brgy. Santo Cristo,</b></div>
+                <div><b>1105 Quezon City, Philippines</b></div>
+              </div>
+            </div>  
+          </div>  
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+    </div>
+<!-- END OF MOBILE FOOTER -->
   </q-layout>
 </template>
 
